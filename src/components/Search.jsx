58245244cc00607de3a81163d711b38281139786
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2025-10-17 09:45:27 +0800

    Edits

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,14 @@
-export default function Search() {
+import { useState } from 'react';
+
+export default function Search(props) {
+  const [input, setInput] = useState("")
+
+  const updateInput = (event) =>{
+    const newInput = event.target.value
+    setInput(newInput)
+  }
+
+
   return (
     <div className="max-w-sm space-y-3">
       <div>
@@ -8,7 +18,7 @@ export default function Search() {
           Label
         </label>
         <div className="relative flex rounded-lg shadow-sm">
-          <input
+          <input onChange={updateInput} value={input}
             type="text"
             id="hs-trailing-button-add-on-with-icon-and-button"
             name="hs-trailing-button-add-on-with-icon-and-button"
@@ -30,7 +40,11 @@ export default function Search() {
               <path d="m21 21-4.3-4.3" />
             </svg>
           </div>
-          <button 
+          <button onClick={() => {
+            console.log("here");
+            
+            props.submitSearch(input)
+          }}
             type="button"
             className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
             Search
